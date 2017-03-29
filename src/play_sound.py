43b1e6679125d3b0924bebea68f4b031275fc995
tyler-shen <tshen@ualberta.ca>
2017-03-29 16:04:05 -0600

    sound done

--- src/play_sound.py
+++ src/play_sound.py
@@ -4,13 +4,16 @@ import rospy
 import numpy as np
 from kobuki_msgs.msg import Sound
 
-rospy.init_node('play_sound')
-pub = rospy.Publisher('mobile_base/commands/sound', Sound, queue_size=1)
-sound = Sound()
-sound.value = 0
+def main():
+    rospy.init_node('play_sound')
+    pub = rospy.Publisher('/mobile_base/commands/sound', Sound, queue_size = 1)
+    sound = Sound()
+    sound.value = 0
 
-print "test"
-print(sound)
-pub.publish(sound)
+    while not rospy.is_shutdown():
+        pub.publish(sound)
+        rospy.sleep(0.5)
 
-# rospy.spin()
+
+if __name__ == "__main__":
+	main()
